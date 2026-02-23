f524afbef5bb5a3b5c7b41388e92a5079108c77b
gurleen20kaur <gkwahla@mun.ca>
2026-02-23 16:05:08 -0330

    function rendering

--- React-learning-app/src/components/ListGroup.tsx
+++ React-learning-app/src/components/ListGroup.tsx
@@ -1,8 +1,13 @@
 function ListGroup() {
-  const cities = ["Delhi", "New york", "Paris", "Ottowa", "Karachi"];
+  // const cities = ["Delhi", "New york", "Paris", "Ottowa", "Karachi"];
+  const cities: string[] = [];
+  const getMessage = () => {
+    return cities.length === 0 ? <p>List is empty</p> : null;
+  };
   return (
     <>
       <h1>List Group</h1>
+      {getMessage()}
       <ul className="list-group">
         {cities.map((item) => (
           <li key={item}>{item}</li>
