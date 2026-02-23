ccdc1f74b14a6b3457329f3b35f91b4e542488ef
gurleen20kaur <gkwahla@mun.ca>
2026-02-23 16:07:29 -0330

    logical and instead of ternary operator

--- React-learning-app/src/components/ListGroup.tsx
+++ React-learning-app/src/components/ListGroup.tsx
@@ -2,7 +2,8 @@ function ListGroup() {
   // const cities = ["Delhi", "New york", "Paris", "Ottowa", "Karachi"];
   const cities: string[] = [];
   const getMessage = () => {
-    return cities.length === 0 ? <p>List is empty</p> : null;
+    // better way then ternary operator, use logical and
+    return cities.length === 0 && <p>List is empty</p>;
   };
   return (
     <>
