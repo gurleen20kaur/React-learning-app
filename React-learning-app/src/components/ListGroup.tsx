c28e6670c8955da15e32e5148b6782ee2f6af595
gurleen20kaur <gkwahla@mun.ca>
2026-02-24 14:42:54 -0330

    Managing State hooks

--- React-learning-app/src/components/ListGroup.tsx
+++ React-learning-app/src/components/ListGroup.tsx
@@ -1,30 +1,31 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const cities = ["Delhi", "New york", "Paris", "Ottowa", "Karachi"];
-  // const cities: string[] = [];
+
+  // hook
+  const [selected, setSelected] = useState(-1);
+
   const getMessage = () => {
     // better way then ternary operator, use logical and
     return cities.length === 0 && <p>List is empty</p>;
   };
 
-  // type annotation in typescript
-  // event handler
-  // function will be called at runtime
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
   return (
     <>
       <h1>List Group</h1>
       {getMessage()}
       <ul className="list-group">
-        {cities.map((item) => (
+        {cities.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selected === index ? "list-group-item active" : "list-group-item"
+            }
             key={item}
             // Pass a reference and not call the function
-            onClick={handleClick}
+            onClick={() => {
+              setSelected(index);
+            }}
           >
             {item}
           </li>
