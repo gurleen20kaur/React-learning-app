dd5bc6a51b843b697d142510eff68f705e4b7c2a
gurleen20kaur <gkwahla@mun.ca>
2026-02-25 19:21:53 -0330

    Using StateHook instead of useRef

--- React-learning-app/src/components/Form.tsx
+++ React-learning-app/src/components/Form.tsx
@@ -1,32 +1,41 @@
-import { useRef, type FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
+  // using state hooks
+  const [person, setPerson] = useState({
     name: "",
     age: 0,
-  };
+  });
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current != null) person.name = nameRef.current.value;
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
   };
   return (
-    // eslint-disable-next-line react-hooks/immutability
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
