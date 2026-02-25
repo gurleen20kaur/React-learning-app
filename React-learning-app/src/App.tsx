aaa652ad80c2a9bb87e68165800d9a52d7cd25ba
gurleen20kaur <gkwahla@mun.ca>
2026-02-25 19:03:56 -0330

    Accessing Input Fields

--- React-learning-app/src/App.tsx
+++ React-learning-app/src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
-  const BtnText = "My Button";
-  const [alertVisible, setAlertVisible] = useState(false);
-
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>
-          Hello <span>World</span>
-        </Alert>
-      )}
-      <Button text={BtnText} onClick={() => setAlertVisible(true)}></Button>
+      <Form />
     </div>
   );
 }
