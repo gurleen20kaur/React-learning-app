90334590b1cbd41860eeb81d0c9107f46cdce133
gurleen20kaur <gkwahla@mun.ca>
2026-02-26 19:03:35 -0330

    Defined a schema for the form using zod

--- React-learning-app/src/components/Form.tsx
+++ React-learning-app/src/components/Form.tsx
@@ -1,18 +1,20 @@
 import { useForm, type FieldValues } from "react-hook-form";
+import { z } from "zod";
 
-// use an interface to define shape of this form
-// and let TS know about our input fields
-interface FormData {
-  name: string;
-  age: number;
-}
+// store all the validation rules
+const schema = z.object({
+  name: z.string().min(3),
+  age: z.number().min(18),
+});
+
+// zod has a method that allows us to extract a type from the schema object
+// don't need interface
+// <typeof schema> return TS type, similar to an interface
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
+  const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -23,17 +25,17 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
+        {/* {errors.name?.type === "required" && (
           <p className="text-danger">The name field is required</p>
         )}
         {errors.name?.type === "minLength" && (
           <p className="text-danger">The name must be atleast 3 characters</p>
-        )}
+        )} */}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
